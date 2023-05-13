cfd8ad927100d05c13be5e8e97bfa5cd7488f996
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2023-05-13 15:46:48 +0200

    Remove the double zero in the hexValues constant

--- components/PaletteCards.js
+++ components/PaletteCards.js
@@ -15,7 +15,7 @@ const paletteCards = {
     `,
     methods: {
         generateRandomColors() {
-            const hexValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F'];
+            const hexValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
             let color = '#';
 
             for (let i = 0; i <= 5; i++) {
